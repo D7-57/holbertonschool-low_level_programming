9cab94e1f9b63d1dfe54bcb7cc70637a1c132a3b
D7-57 <10665@holbertonstudents.com>
2025-02-17 10:49:08 +0000

    T

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -12,7 +12,6 @@
 int main(void)
 {
 	const char *message="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-
 	write(2,message,59); /* Output the message to stderr */
 	return (1);
 }
