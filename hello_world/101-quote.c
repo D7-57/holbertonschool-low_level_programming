f3672669661db768598e5633a7e654dc4974a245
D7-57 <10665@holbertonstudents.com>
2025-02-17 10:35:49 +0000

    T

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -1,2 +1,16 @@
-#!/bin/bash
-echo "and that piece of art is useful\" - Dora Korpar, 2015-10-19" >&2
+#include <unistd.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: This program prints a specific quote to the standard error
+ * without using printf or puts, and returns 1.
+ *
+ * Return: Always 1 (Error)
+ */
+int main(void)
+{
+    char *quote = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+    write(2, quote, 59);
+    return (1);
+}
