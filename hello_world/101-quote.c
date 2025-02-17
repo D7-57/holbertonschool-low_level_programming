decfcdd503a40a546c00a429bbd26dc86bd7f896
D7-57 <10665@holbertonstudents.com>
2025-02-17 10:45:28 +0000

    T

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -4,14 +4,15 @@
  * main - Entry point of the program
  *
  * Description: Prints a message to the standard error and returns 1.
- *              The message is: "and that piece of art is useful\" - Dora Korpar, 2015-10-19"
- *              followed by a new line.
+ *              The message is: "and that piece of art is useful\" -
+ *              Dora Korpar, 2015-10-19" followed by a new line.
  *
  * Return: Always 1 (indicating an error)
  */
 int main(void)
 {
     const char *message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
     write(2, message, 59); /* 59 is the length of the message including the newline */
     return (1);
 }
