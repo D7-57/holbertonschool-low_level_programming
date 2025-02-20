bbc426c377b828867c70365eddbfcdd96dc9558e
D7-57 <10665@holbertonstudents.com>
2025-02-20 16:28:20 +0000

    T

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,6 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/**
+ * _putchar - Writes a character to stdout
+ * @c: The character to print
+ * Return: 1 on success, -1 on error
+ */
 int _putchar(char c);
+
+/**
+ * print_alphabet - Prints the alphabet in lowercase
+ */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
