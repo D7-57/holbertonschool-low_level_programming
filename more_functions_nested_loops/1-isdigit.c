62e558cf27412ac98c1b71480bbf51b3c8255cfd
D7-57 <10665@holbertonstudents.com>
2025-02-27 09:38:53 +0000

    T

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -4,11 +4,14 @@
  * _isdigit - Checks if a character is a digit (0-9)
  * @c: The character to check
  *
+ * Description: This function takes an integer representing an ASCII
+ * character and determines whether it is a numeric digit (0-9).
+ *
  * Return: 1 if c is a digit, 0 otherwise
  */
 int _isdigit(int c)
 {
-    if (c >= '0' && c <= '9') /* ASCII values: 48 to 57 */
-        return (1);
-    return (0);
+	if (c >= '0' && c <= '9') /* ASCII values: 48 to 57 */
+		return (1);
+	return (0);
 }
