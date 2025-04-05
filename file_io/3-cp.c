2c2d093d14c4d1de5061403bcd4d50729565fce1
D7-57 <10665@holbertonstudents.com>
2025-04-05 12:43:57 +0000

    h

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,7 @@
 /**
  * print_error - Prints an error message to stderr and exits
  * @code: Exit code
- * @msg: Error message format (must include %s if using @arg)
+ * @msg: Error message format
  * @arg: Argument to include in the message (e.g. file name)
  */
 void print_error(int code, const char *msg, const char *arg)
@@ -23,7 +23,7 @@ void print_error(int code, const char *msg, const char *arg)
  * @ac: Argument count
  * @av: Argument vector
  *
- * Return: 0 on success, exits with appropriate error code on failure
+ * Return: 0 on success, exits with error code on failure
  */
 int main(int ac, char **av)
 {
@@ -47,8 +47,18 @@ int main(int ac, char **av)
 		print_error(99, "Error: Can't write to %s\n", av[2]);
 	}
 
-	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
+	while (1)
 	{
+		r_bytes = read(fd_from, buffer, BUF_SIZE);
+		if (r_bytes == -1)
+		{
+			close(fd_from);
+			close(fd_to);
+			print_error(98, "Error: Can't read from file %s\n", av[1]);
+		}
+		if (r_bytes == 0)
+			break;
+
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
@@ -58,19 +68,11 @@ int main(int ac, char **av)
 		}
 	}
 
-	if (r_bytes == -1)
-	{
-		close(fd_from);
-		close(fd_to);
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
-	}
-
 	if (close(fd_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
