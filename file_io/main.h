5f7c95274d95f5161fadc8b092f3e818e1ead1ca
D7-57 <10665@holbertonstudents.com>
2025-04-05 13:15:25 +0000

    T

--- file_io/main.h
+++ file_io/main.h
@@ -3,6 +3,7 @@
 
 #include <sys/types.h>
 #include <stddef.h>
+#include <elf.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
