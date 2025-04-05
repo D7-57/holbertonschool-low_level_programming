842a28376dbfcccb2197c964fc03b2f6d02ef144
D7-57 <10665@holbertonstudents.com>
2025-04-05 12:51:35 +0000

    gggg

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -7,18 +7,6 @@
 #define BUF_SIZE 1024
 
 /**
- * print_error - Prints an error message to stderr and exits
- * @code: Exit code
- * @msg: Error message format
- * @arg: Argument to include in the message
- */
-void print_error(int code, const char *msg, const char *arg)
-{
-	dprintf(STDERR_FILENO, msg, arg);
-	exit(code);
-}
-
-/**
  * main - Copies the content of a file to another file
  * @ac: Argument count
  * @av: Argument vector
@@ -38,31 +26,37 @@ int main(int ac, char **av)
 
 	fd_from = open(av[1], O_RDONLY);
 	if (fd_from == -1)
-		print_error(98, "Error: Can't read from file %s\n", av[1]);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
 
 	fd_to = open(av[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (fd_to == -1)
 	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(fd_from);
-		print_error(99, "Error: Can't write to %s\n", av[2]);
+		exit(99);
 	}
 
-	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) != 0)
+	while ((r_bytes = read(fd_from, buffer, BUF_SIZE)) > 0)
 	{
-		if (r_bytes == -1)
-		{
-			close(fd_from);
-			close(fd_to);
-			print_error(98, "Error: Can't read from file %s\n", av[1]);
-		}
-
 		w_bytes = write(fd_to, buffer, r_bytes);
 		if (w_bytes == -1 || w_bytes != r_bytes)
 		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 			close(fd_from);
 			close(fd_to);
-			print_error(99, "Error: Can't write to %s\n", av[2]);
+			exit(99);
 		}
+	}
+
+	if (r_bytes == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]);
+		close(fd_from);
+		close(fd_to);
+		exit(98);
 	}
 
 	if (close(fd_from) == -1)
@@ -70,7 +64,6 @@ int main(int ac, char **av)
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
 	if (close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
